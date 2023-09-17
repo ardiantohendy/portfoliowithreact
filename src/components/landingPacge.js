46bf9f519a3dee71fd9698de1314cfb022fcf96f
Ardiantohendy <bayuardianto421@gmail.com>
2023-09-17 21:49:16 +0700

    new commit

--- src/components/landingPacge.js
+++ src/components/landingPacge.js
@@ -6,8 +6,14 @@ function LandingPage(params) {
       <div className="child-image"></div>
       <div className="child-desc">
         <h2>WanaCode</h2>
+        <h2 className="shadow">WanaCode</h2>
         <p>terpecaya dalam pengembangan aplikasi web dan mobile</p>
-        <a href="#">Portfolio</a>
+        <p>lorem ipsum dolor sit amet consectetur</p>
+        <p>lorem ipsum dolor sit amet</p>
+        <p>lorem ipsum dolor sit</p>
+        <p>lorem, ipsum dolor</p>
+        <p>lorem, ipsum</p>
+        <p>lorem.</p>
       </div>
     </div>
   );
